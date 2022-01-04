deb942ead8aebe8de73ff747085747806a260956
nick-here <zuraniewski.karol@gmail.com>
2022-01-04 13:15:06 +0100

    Comment jsx

---
+++ src/components/thread/Comment.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Comment.css';
+import Content from '../Content';
+import ActionBtn from './ActionBtn';
+import KarmaCounter from './KarmaCounter';
+import UserData from './UserData';
+
+const breakpoints = {
+	sm: 576,
+	md: 768,
+	lg: 992,
+	xl: 1200,
+	xxl: 1400,
+};
+
+export default function Comment({ img, username, createdAt, text, ...other }) {
+	const isDesktop = window.screen.width >= breakpoints.lg;
+
+	console.log(isDesktop);
+
+	return (
+		<div className='Comment' {...other}>
+			{isDesktop && <KarmaCounter />}
+
+			<div>
+				<div className='Comment__header'>
+					<UserData img={img} username={username} createdAt={createdAt} />
+					<ActionBtn theme='reply' text='Reply' />
+				</div>
+
+				<Content text={text} />
+			</div>
+
+			{!isDesktop && <KarmaCounter />}
+		</div>
+	);
+}
