575a65361ca24c167f8f9d5392a877b68e3078f5
nick-here <zuraniewski.karol@gmail.com>
2022-01-13 21:29:58 +0100

    TextInput using react-textarea-autosize instead of autosize

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,24 +1,17 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import Avatar from './Avatar';
 import './TextInput.css';
 import Btn from './Btn';
-import autosize from 'autosize';
 
 export default function TextInput({ img, placeholder = '' }) {
-	const ref = useRef(null);
-
-	useEffect(() => {
-		autosize(ref.current);
-	}, [ref]);
-
 	return (
 		<div className='TextInput'>
-			<textarea
-				ref={ref}
+			<TextareaAutosize
 				className='TextInput__textarea'
 				name='comment'
 				placeholder={placeholder}
-			></textarea>
+			></TextareaAutosize>
 
 			<Avatar src={img} />
 
