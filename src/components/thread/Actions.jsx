151d7847277e822df72845430ca793243e122a53
nick-here <zuraniewski.karol@gmail.com>
2022-01-14 20:23:27 +0100

    properties as btn callbacks

--- src/components/thread/Actions.jsx
+++ src/components/thread/Actions.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Actions.css';
 import ActionBtn from './ActionBtn';
 
-export default function Actions({ reply = false, edit = false, delete: del = false }) {
+export default function Actions({ reply = null, edit = null, delete: del = null }) {
 	return (
 		<div className='Actions'>
-			{del && <ActionBtn theme='delete' text='Delete' />}
-			{edit && <ActionBtn theme='edit' text='Edit' />}
-			{reply && <ActionBtn theme='reply' text='Reply' />}
+			{del && <ActionBtn theme='delete' text='Delete' onClick={del} />}
+			{edit && <ActionBtn theme='edit' text='Edit' onClick={edit} />}
+			{reply && <ActionBtn theme='reply' text='Reply' onClick={reply} />}
 		</div>
 	);
 }
