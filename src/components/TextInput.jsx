e32a3a7a84018b2e85d36d4d3dfd120426435c23
nick-here <zuraniewski.karol@gmail.com>
2022-01-15 00:17:00 +0100

    saving text to localState

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import Avatar from './Avatar';
 import './TextInput.css';
 import Btn from './Btn';
 
 export default function TextInput({ img, placeholder = '' }) {
+	const [value, setValue] = useState(localStorage.getItem('typedContent'));
+
 	return (
 		<div className='TextInput'>
 			<TextareaAutosize
 				className='TextInput__textarea'
 				name='comment'
 				placeholder={placeholder}
+				value={value}
+				onChange={(e) => {
+					setValue(e.target.value);
+					localStorage.setItem('typedContent', e.target.value);
+				}}
 			></TextareaAutosize>
 
 			<Avatar src={img} />
