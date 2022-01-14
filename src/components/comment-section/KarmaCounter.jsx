c04f11216e6efab64fa6b31b5299e9609774cc06
nick-here <zuraniewski.karol@gmail.com>
2022-01-15 00:49:34 +0100

    removed temporary liking functionality

--- src/components/comment-section/KarmaCounter.jsx
+++ src/components/comment-section/KarmaCounter.jsx
@@ -13,16 +13,14 @@ function Btn({ src, ...other }) {
 
 //TODO: proper liking/disliking
 
-export default function KarmaCounter({ karma: initialKarma = 0 }) {
-	const [karma, setKarma] = useState(initialKarma);
-
+export default function KarmaCounter({ karma = 0 }) {
 	return (
 		<div className='KarmaCounter'>
-			<Btn src={plusIcon} onClick={() => setKarma((karma) => karma + 1)} />
+			<Btn src={plusIcon} />
 
 			<div className='KarmaCounter__counter'>{karma}</div>
 
-			<Btn src={minusIcon} onClick={() => setKarma((karma) => karma - 1)} />
+			<Btn src={minusIcon} />
 		</div>
 	);
 }
