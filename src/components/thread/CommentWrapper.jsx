c98deaf3a36777b5829f8c42298864913b047d43
nick-here <zuraniewski.karol@gmail.com>
2022-01-14 23:44:04 +0100

    reply form functional + saving text to localStorage

--- src/components/thread/CommentWrapper.jsx
+++ src/components/thread/CommentWrapper.jsx
@@ -5,11 +5,14 @@ import TextInput from '../TextInput';
 import data from '../../data';
 
 export default function CommentWrapper({ commentData }) {
-	const [replyMode, setReplyMode] = useState(false);
+	const [replyMode, setReplyMode] = useState(localStorage.getItem('typedContent').length > 0);
 
 	return (
 		<div className='CommentWrapper'>
-			<Comment commentData={commentData} />
+			<Comment
+				commentData={commentData}
+				onReply={() => setReplyMode((replyMode) => !replyMode)}
+			/>
 
 			{replyMode && (
 				<TextInput img={data.currentUser.image.png} placeholder='Add a reply...' />
