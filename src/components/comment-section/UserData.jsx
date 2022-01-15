35a2fbe79e3f3c7b5d2a5a99040b86cd1b13aa64
nick-here <zuraniewski.karol@gmail.com>
2022-01-15 01:29:06 +0100

    'you' indicator

--- src/components/comment-section/UserData.jsx
+++ src/components/comment-section/UserData.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Avatar from '../Avatar';
 import Reference from '../Reference';
 import './UserData.css';
+import data from '../../data';
 
 export default function UserData({ img, username, createdAt }) {
 	return (
 		<div className='UserData'>
 			<Avatar src={img} />
+
 			<Reference username={username} />
+
+			{username === data.currentUser.username && <div className='UserData__you'>you</div>}
+
 			<div className='UserData__created-at'>{createdAt}</div>
 		</div>
 	);
