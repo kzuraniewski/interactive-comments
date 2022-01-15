96ca55fe212853fb103393de86151e3e421872d0
nick-here <zuraniewski.karol@gmail.com>
2022-01-15 01:48:09 +0100

    currentUser as a prop

--- src/components/comment-section/UserData.jsx
+++ src/components/comment-section/UserData.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import Avatar from '../Avatar';
 import Reference from '../Reference';
 import './UserData.css';
-import data from '../../data';
 
-export default function UserData({ img, username, createdAt }) {
+export default function UserData({ img, username, createdAt, currentUser }) {
 	return (
 		<div className='UserData'>
 			<Avatar src={img} />
 
 			<Reference username={username} />
 
-			{username === data.currentUser.username && <div className='UserData__you'>you</div>}
+			{currentUser && <div className='UserData__you'>you</div>}
 
 			<div className='UserData__created-at'>{createdAt}</div>
 		</div>
