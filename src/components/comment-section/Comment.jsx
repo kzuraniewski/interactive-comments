fb0af176f8bfed364121a23969bbcd2afbbb2168
nick-here <zuraniewski.karol@gmail.com>
2022-01-15 15:15:12 +0100

    edit mode without functionality

--- src/components/comment-section/Comment.jsx
+++ src/components/comment-section/Comment.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Comment.css';
 import Content from '../Content';
 import Actions from './Actions';
 import KarmaCounter from './KarmaCounter';
 import UserData from './UserData';
 import data from '../../data';
+import Textarea from '../Textarea';
+import Btn from '../Btn';
 
 const breakpoints = {
 	sm: 576,
@@ -15,7 +17,6 @@ const breakpoints = {
 };
 
 export default function Comment({ comment, onReply = null, ...other }) {
-	const isDesktop = window.screen.width >= breakpoints.lg;
 	const {
 		content,
 		createdAt,
@@ -27,7 +28,13 @@ export default function Comment({ comment, onReply = null, ...other }) {
 		replyingTo,
 	} = comment;
 
+	// check if the comment belongs to the current user
 	const currentUser = username === data.currentUser.username;
+	// check if the device is desktop sized
+	const isDesktop = window.screen.width >= breakpoints.lg;
+
+	const [editMode, setEditMode] = useState(false);
+	const [textareaValue, setTextareaValue] = useState(content);
 
 	return (
 		<div className='Comment' {...other}>
@@ -35,7 +42,7 @@ export default function Comment({ comment, onReply = null, ...other }) {
 				<>
 					<KarmaCounter karma={score} />
 
-					<div>
+					<div className='Comment__content-wrapper'>
 						<div className='Comment__strip'>
 							<UserData
 								img={img}
@@ -44,14 +51,31 @@ export default function Comment({ comment, onReply = null, ...other }) {
 								currentUser={currentUser}
 							/>
 
+							{/* edit & delete for currentUser, reply otherwise */}
 							{currentUser ? (
-								<Actions delete={() => {}} edit={() => {}} />
+								<Actions
+									delete={() => {}}
+									edit={() => setEditMode((editMode) => !editMode)}
+								/>
 							) : (
 								<Actions reply={onReply} />
 							)}
 						</div>
 
-						<Content text={replyingTo ? `@${replyingTo} ` + content : content} />
+						{editMode ? (
+							<>
+								<Textarea
+									content={content}
+									value={textareaValue}
+									setValue={setTextareaValue}
+								/>
+
+								<Btn text='UPDATE' onClick={() => setEditMode(false)} />
+								{/* doesn't update content for now */}
+							</>
+						) : (
+							<Content text={replyingTo ? `@${replyingTo} ` + content : content} />
+						)}
 					</div>
 				</>
 			) : (
@@ -69,7 +93,10 @@ export default function Comment({ comment, onReply = null, ...other }) {
 						<KarmaCounter karma={score} />
 
 						{currentUser ? (
-							<Actions delete={() => {}} edit={() => {}} />
+							<Actions
+								delete={() => {}}
+								edit={() => setEditMode((editMode) => !editMode)}
+							/>
 						) : (
 							<Actions reply={onReply} />
 						)}
