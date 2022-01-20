56c719cc56c23db0f5d15a5de81cb672842057db
nick-here <zuraniewski.karol@gmail.com>
2022-01-20 13:54:27 +0100

    modal component

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import Btn from './comment-section/Btn';
 import './Modal.css';
 
-export default function Modal({ title, description = '', cancel = 'CANCEL', confirm = 'CONFIRM' }) {
+export default function Modal({ isOpen, title, description = '', cancel = 'CANCEL', confirm = 'CONFIRM', onConfirm, onCancel }) {
 	return (
-		<div className='Modal'>
-			<h1 className='Modal__title'>{title}</h1>
+		<div className='Modal' style={{ display: isOpen ? '' : 'none' }} onClick={onCancel}>
+			<div className='Modal__content' onClick={e => e.stopPropagation()}>
+				<h1 className='Modal__title'>{title}</h1>
 
-			{description && <p className='Modal__description'>{description}</p>}
+				{description && <p className='Modal__description'>{description}</p>}
 
-			<div className='Modal__actions'>
-				<Btn text={cancel} theme='cancel' />
+				<div className='Modal__actions'>
+					<Btn text={cancel} theme='cancel' />
 
-				<Btn text={confirm} theme='danger' />
+					<Btn text={confirm} theme='danger' onClick={onConfirm} />
+				</div>
 			</div>
 		</div>
 	);
