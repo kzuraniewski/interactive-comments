3c17fad7472d9f68a1eff3bd40a473895de84f67
nick-here <zuraniewski.karol@gmail.com>
2022-08-09 11:13:04 +0200

    modal opened via context

---
+++ src/comment-section/Modal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Btn from './Btn';
+import './Modal.css';
+
+export default function Modal({
+	isOpen,
+	title,
+	description = '',
+	cancel = 'CANCEL',
+	confirm = 'CONFIRM',
+	onConfirm,
+	onCancel,
+}) {
+	return (
+		<div className='Modal' style={{ display: isOpen ? '' : 'none' }} onClick={onCancel}>
+			<div className='Modal__content' onClick={e => e.stopPropagation()}>
+				<h1 className='Modal__title'>{title}</h1>
+
+				{description && <p className='Modal__description'>{description}</p>}
+
+				<div className='Modal__actions'>
+					<Btn text={cancel} theme='cancel' onClick={onCancel} />
+
+					<Btn text={confirm} theme='danger' onClick={onConfirm} />
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/comment-section/ModalProvider.jsx
@@ -0,0 +1,47 @@
+import React, { useCallback, useState } from 'react';
+import Modal from './Modal';
+
+export const ModalContext = React.createContext(null);
+
+const defaultOptions = {
+	title: 'Modal',
+	cancel: 'No, cancel',
+	confirm: 'Yes, confirm',
+};
+
+export default function ModalProvider({ children }) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [options, setOptions] = useState(defaultOptions);
+
+	const openModal = newOptions => {
+		setIsModalOpen(true);
+
+		for (const key of Object.keys[defaultOptions]) {
+			newOptions[key] ??= defaultOptions[key];
+		}
+
+		setOptions(newOptions);
+	};
+
+	// modal always closes when one of its buttons is pressed
+	const handleEvent = useCallback(
+		cb => () => {
+			cb?.();
+			setIsModalOpen(false);
+		},
+		[]
+	);
+
+	return (
+		<ModalContext.Provider value={openModal}>
+			{children}
+
+			<Modal
+				isOpen={isModalOpen}
+				onConfirm={handleEvent(options.onConfirm)}
+				onCancel={handleEvent(options.onCancel)}
+				{...options}
+			/>
+		</ModalContext.Provider>
+	);
+}
