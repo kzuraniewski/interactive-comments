c4a3139f24edbdb08413831338ae1108fd02785a
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-02-09 15:54:33 +0100

    text input migrated to ts

--- src/comment-section/TextInput.tsx
+++ src/comment-section/TextInput.tsx
@@ -4,19 +4,32 @@ import './TextInput.css';
 import Btn from './Btn';
 import Textarea from './Textarea';
 
+export type TextInputProps = {
+	img: string;
+	placeholder?: string;
+	initialValue?: string;
+	focus?: boolean;
+	onSend?: () => void;
+};
+
 export default function TextInput({
 	img,
-	onSend,
 	placeholder = '',
 	initialValue = '',
 	focus = false,
-}) {
+	onSend,
+}: TextInputProps) {
 	const [value, setValue] = useState('');
 
 	// Add initial value after mount so when focused, the cursor is at the end of the line
 	useEffect(() => {
 		setValue(initialValue);
 	}, [initialValue]);
+
+	const handleSend = () => {
+		if (!value.length) return;
+		onSend?.();
+	};
 
 	return (
 		<div className='TextInput'>
@@ -29,13 +42,7 @@ export default function TextInput({
 
 			<Avatar src={img} />
 
-			<Btn
-				text='SEND'
-				onClick={e => {
-					if (!value.length) return;
-					onSend(e);
-				}}
-			/>
+			<Btn text='SEND' onClick={handleSend} />
 		</div>
 	);
 }
